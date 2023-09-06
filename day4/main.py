9007ab0842e67f15ff522dc0f6bf177b523d364a
iq2v <142683864+iq2v@users.noreply.github.com>
2023-09-06 20:01:14 +0300

    Create main.py

---
+++ day4/main.py
@@ -0,0 +1,18 @@
+def my_name () : 
+ print("my name is jana")
+
+my_name ()
+
+def my_meal(food,drink) :
+ print("i like to eat" ,food, " and while drinking ", drink)
+  my_meal(food,drink)
+ def cube(number) : 
+  return number**3
+ 
+ def by_three(number) : 
+  if number %3==0:
+    return cube  (number)
+  else :
+    return False 
+  print(by_three(3))
+ 
