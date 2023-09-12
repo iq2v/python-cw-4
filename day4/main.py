839150ab133ca804c805e1bd9233c6816481c5f3
iq2v <142683864+iq2v@users.noreply.github.com>
2023-09-12 22:15:43 +0300

    Update main.py

--- day4/main.py
+++ day4/main.py
@@ -1,18 +1,28 @@
-def my_name () : 
- print("my name is jana")
+#الجزء الاول
+def my_name() :
+    print('My name is jana')
+my_name()
 
-my_name ()
+#الجزء الثاني
+def my_meal(food , drink) :
+    print(f'i like to eat {food } while drinking {drink}')
 
-def my_meal(food,drink) :
- print("i like to eat" ,food, " and while drinking ", drink)
-  my_meal(food,drink)
- def cube(number) : 
-  return number**3
+my_meal('popcorn','cola')
+
+#الجزء الثالث
+def cube(number) :
+    return number **3
+result = cube(4)
+print(result)
  
- def by_three(number) : 
-  if number %3==0:
-    return cube  (number)
-  else :
-    return False 
-  print(by_three(3))
- 
+def by_three(number) :
+    if number %3 == 0 :
+        return cube(number)
+    else:
+         False
+
+example1 = by_three(9)
+print(example1)
+
+example2 = by_three(4)
+print(example2)
